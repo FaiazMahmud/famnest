48432a9423758d142797d5f7d493d4030dce72d6
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2024-12-17 15:43:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from jose import jwt, JWTError
 import os
 from fastapi.middleware.cors import CORSMiddleware
 
-# Initialize FastAPI app
+# creating a FastAPI instance
 app = FastAPI()
 
 app.add_middleware(
@@ -19,19 +19,17 @@ app.add_middleware(
 )
 
 
-# MongoDB connection string (replace with your MongoDB Atlas credentials)
+# MongoDB connection string (filling up with MongoDB Atlas credentials)
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
-db = client.get_database('FamNest')  # Replace with your database name
-collection1 = db.get_collection('email-password')  # Replace with your collection name
+db = client.get_database('FamNest')  
+# collection1 = db.get_collection('email-password') 
 
 
-# Define the Pydantic model to handle incoming data
+# Defining the Pydantic model to handle incoming data
 class InputText(BaseModel):
     input_text: str
 
-
-# Endpoint to check if the input already exists in the database and insert if not
 
 # for user_registration
 # name , email , password , joinCreate(by default : false) class 
@@ -39,7 +37,8 @@ class register(BaseModel):
     name: str
     email: str
     password: str
-    
+
+# Endpoint
 @app.post("/save-input/")
 async def save_input(info: register):
     name = info.name
@@ -77,3 +76,44 @@ async def check_input(info : login):
         return {"message": "Login Successful", "status": "success"}  # Success message if inserted
     else:
         return {"message": "Incorrect Email or Password", "status": "success"}  # Return error if already exists
+
+# for creation of groups (collection = logged in successfully , {name , email , AtLeastOneGroup , groups})
+# called from create join page
+class groupCreate(BaseModel):
+   email : str
+   new_group : str
+
+@app.post("/group-create/")
+async def group_create(info : groupCreate):
+   email = info.email
+   new_group = info.new_group
+   User = db.get_collection("Registerd_Users_Only")
+   User_Info = User.find_one({"email": email})  # Finding user by email
+    
+   if not User_Info:
+        return {"error": "User not found with the given email"}
+    
+   # Extracting the required details from the User_INFO document
+   name = User_Info.get("name")
+   password = User_Info.get("password")
+   collection = db.get_collection("Users Logged in Successfully with a Group")
+   collection.update_one(
+        {"name" : name}, # Storing the name of user 
+        {"email": email}, 
+        {"password":password}, # Match the document by email
+        {"AtLeastOneGroup" : True},
+        {"$push": {"groups": {"$each": new_group}}},  # Add new groups to the array
+        upsert=True  # Create the document if it doesn't exist
+   )
+
+# for checking whether to show Join Create Group page or not
+# called from login page
+class email(BaseModel):
+    email : str
+@app.post("/check-one-group-criteria/")
+async def check_one_group_criteria(info : email):
+    email = info.email
+    collection = db.get_collection("Users Logged in Successfully with a Group")
+    existing_entry = collection.find_one({"email":email})
+    return {"exists": bool(existing_entry)}
+   
