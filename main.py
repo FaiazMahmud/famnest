bfa0ae7f7d83c9c8e357f7b1ccdfd1bf1f8a6b38
Md. Shahriar Kabir <159224133+shahriarkabir280@users.noreply.github.com>
2025-01-27 21:27:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from jose import jwt, JWTError
 import os
 from fastapi.middleware.cors import CORSMiddleware
-
+from magic import from_buffer
 from typing import Optional, List
 from bson import ObjectId
 
